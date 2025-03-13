4db09759f59631845e8a18774c3a1be3eb01e61a
sathikommiti <sathish.kommiti@gmail.com>
2025-03-13 23:31:38 +0530

    abc

---
+++ git_practice/Thursday_nyt.java
@@ -0,0 +1,14 @@
+package git_practice;
+
+public class Thursday_nyt {
+    static int i=10,j=10;
+    public static void main(String[] args) {
+        if(i==j){
+            System.out.println("True");
+        }
+    }
+
+
+
+
+}
