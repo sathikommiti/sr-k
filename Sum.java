0ce3091ce12f13df26a5c8ef4986c15e81ab5975
sathikommiti <sathish.kommiti@gmail.com>
2025-03-19 18:32:44 +0530

    sample code is commited

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+static int i = 100, j = 1000,k;
+    public static void main(String[] args) {
+
+        k = i+j;
+        System.out.println("The value of K is: "+k);
+    }
+
+}
