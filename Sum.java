e2c337eb885a3a8f1481e7b498bd069d6fb808d2
sathikommiti <sathish.kommiti@gmail.com>
2025-03-19 19:11:14 +0530

    main

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
 static int i = 100, j = 1000,k;
     public static void main(String[] args) {
 
-        k = i+j;
+        k = i+j+2;
         System.out.println("The value of K is: "+k);
     }
 
